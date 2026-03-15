c3a4df9e4b7e3a5d3f05fbe0d077fba3e821674e
supermima <michael.alyaev@icloud.com>
2026-03-15 15:07:45 +0100

    To spilere PC

--- main.ts
+++ main.ts
@@ -1,27 +1,8 @@
 input.onButtonPressed(Button.A, function () {
     start()
 })
-input.onGesture(Gesture.Shake, function () {
-    bn = randint(0, 2)
-    basic.showString("123")
-})
-input.onButtonPressed(Button.B, function () {
-    bn += 1
-    if (2 < bn) {
-        bn += -3
-    }
-})
-function start () {
-    basic.showString("?")
-    radio.setGroup(1)
-    bn = -1
-    mot = -1
-}
-let mot = 0
-let bn = 0
-start()
-basic.forever(function () {
-    if (bn == 0) {
+function visResultat (tall: number) {
+    if (tall == 0) {
         basic.showLeds(`
             . # # # .
             # # # # #
@@ -29,7 +10,7 @@ basic.forever(function () {
             # # # # #
             . # # # .
             `)
-    } else if (bn == 1) {
+    } else if (tall == 1) {
         basic.showLeds(`
             # # . . #
             # # . # .
@@ -37,7 +18,7 @@ basic.forever(function () {
             # # . # .
             # # . . #
             `)
-    } else if (bn == 2) {
+    } else if (tall == 2) {
         basic.showLeds(`
             . # # # .
             . # # # .
@@ -48,4 +29,51 @@ basic.forever(function () {
     } else {
         basic.showIcon(IconNames.No)
     }
+}
+input.onGesture(Gesture.Shake, function () {
+    mot = randint(0, 2)
+    basic.showString("123")
+    visResultat(mot)
+    basic.pause(1000)
+    resultat(bn, mot)
+})
+input.onButtonPressed(Button.B, function () {
+    bn += 1
+    if (2 < bn) {
+        bn += -3
+    }
+    visResultat(bn)
+})
+function start () {
+    basic.showString("?")
+    radio.setGroup(1)
+    bn = -1
+    mot = -1
+}
+function resultat (du: number, motSpiller: number) {
+    if (du == 0 && motSpiller == 1) {
+        vant = 1
+    } else if (du == 1 && motSpiller == 2) {
+        vant = 1
+    } else if (du == 2 && motSpiller == 0) {
+        vant = 1
+    } else if (du == motSpiller) {
+        vant = 0
+    } else {
+        vant = -1
+    }
+    if (vant == 1) {
+        basic.showString("Du vant!")
+    } else if (vant == -1) {
+        basic.showString("Du tapte!")
+    } else {
+        basic.showString("Uavgjort!")
+    }
+}
+let vant = 0
+let bn = 0
+let mot = 0
+start()
+basic.forever(function () {
+	
 })
